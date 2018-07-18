332a0730c0c0d440339c54b124f4337fa47af890
Harry Green <harry@hgreen.co.uk>
2018-07-18 17:46:47 +0100

    Add a button to the navigation

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -28,6 +28,7 @@ const NavigationItem = ({ children, href, active }) => (
 
 const NavigationMenu = styled.ul`
   display: flex;
+  flex: 1 0 auto;
   height: 48px;
   justify-content: space-around;
   align-items: center;
@@ -35,15 +36,26 @@ const NavigationMenu = styled.ul`
   margin: 0;
   padding: 0;
 `
+const AddButton = styled.button`
+  border: none;
+  background-color: #000;
+  width: 48px;
+  height: 48px;
+  color: #fff;
+  font-size: 20px;
+  line-height: 48px;
+  font-family: 'Roboto Mono';
+`;
 
 const NavigationBar = () => {
   return (
-    <nav>
+    <nav style={{'display': 'flex'}}>
       <NavigationMenu>
         <NavigationItem active href="#read">read</NavigationItem>
         <NavigationItem href="#wishlist">wishlist</NavigationItem>
         <NavigationItem href="#discover">discover</NavigationItem>
       </NavigationMenu>
+      <AddButton>+</AddButton>
     </nav>
   );
 };
