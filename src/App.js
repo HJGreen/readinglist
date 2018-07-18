513ca43b14872814385bb0b3dba5371789e0faa0
Harry Green <harry@hgreen.co.uk>
2018-07-18 23:19:47 +0100

    Add basic list component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 import NavigationBar from './components/NavigationBar';
+import styled from 'styled-components';
+import List from './components/List';
+
+const AppContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  width: 100vw;
+  height: 100vh;
+`
+const Pane = styled.section`
+  flex: 1 0 auto;
+`
+
+const books = [
+  {author: "Kurt Vonnegut", title: "Slaughterhouse 5"}
+]
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
+      <AppContainer>
+        <Pane>
+          <List items={books}/>
+        </Pane>
         <NavigationBar />
-      </div>
+      </AppContainer>
     );
   }
 }
