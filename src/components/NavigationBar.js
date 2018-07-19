0266fe3802f72a9d6e3ae90488603aee0f22227e
Harry Green <harry@hgreen.co.uk>
2018-07-19 19:07:50 +0100

    Formatting

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -56,14 +56,14 @@ const AddButton = styled.button`
 
 const NavigationBar = () => {
   return (
-    <nav style={{'display': 'flex'}}>
+    <nav style={{ 'display': 'flex' }}>
       <NavigationMenu>
         <NavigationItem active href="#read">read</NavigationItem>
         <NavigationItem href="#wishlist">wishlist</NavigationItem>
         <NavigationItem href="#discover">discover</NavigationItem>
       </NavigationMenu>
       <AddButton>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32"   fill="currentColor"><path d="M17 11a1 1 0 0 1 0 2h-4v4a1 1 0 0 1-2 0v-4H7a1 1 0 0 1 0-2h4V7a1 1 0 0 1 2 0v4h4z"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32" fill="currentColor"><path d="M17 11a1 1 0 0 1 0 2h-4v4a1 1 0 0 1-2 0v-4H7a1 1 0 0 1 0-2h4V7a1 1 0 0 1 2 0v4h4z" /></svg>
       </AddButton>
     </nav>
   );
