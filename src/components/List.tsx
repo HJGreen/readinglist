d0b5e0f6f2e12d1a1723b84b1b34ab1835708e7f
Harry Green <harry@hgreen.co.uk>
2019-02-16 18:29:33 +1300

    Style shelf list items

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -15,18 +15,47 @@ const ListContainer = styled.ul`
 
 const ListTitle = styled.p`
   font-weight: 600;
+  font-size: 1.25rem;
   margin: 0 0 0.15rem;
 `;
 
 const ListSubTitle = styled.p`
-  font-size: 0.875rem;
+  font-size: 1rem;
   font-weight: 400;
   margin: 0;
 `;
 
 const StyledListItem = styled.li`
   padding: 0.5rem 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
+const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #222;
+  color: #f2f2f2;
+  text-transform: uppercase;
+  font-size: 1rem;
+  height: 2em;
+  width: 2em;
+  line-height: 2.5em;
+  font-family: inherit;
+  letter-spacing: 0.075em;
+  font-weight: 500;
+  box-shadow: inset 0 0 0 1px #ccc;
+  border: 1px solid #333;
+
+  &:focus {
+    background: #fff;
+    color: #333;
+    border-color: #fff;
+    outline: none;
+  }
+`;
+
 
 type ListItemProps = {
   title: string;
@@ -40,11 +69,15 @@ const ListItem: React.FunctionComponent<ListItemProps> = ({
   onClick
 }) => (
   <StyledListItem>
+      <div>
     <ListTitle>{title}</ListTitle>
     <ListSubTitle>{author}</ListSubTitle>
-    <button type="button" onClick={onClick}>
-      &times;
-    </button>
+      </div>
+    <Button type="button" onClick={onClick}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+            <path fill="currentColor" d="M16.24 14.83a1 1 0 0 1-1.41 1.41L12 13.41l-2.83 2.83a1 1 0 0 1-1.41-1.41L10.59 12 7.76 9.17a1 1 0 0 1 1.41-1.41L12 10.59l2.83-2.83a1 1 0 0 1 1.41 1.41L13.41 12l2.83 2.83z"/>
+        </svg>
+    </Button>
   </StyledListItem>
 );
 
