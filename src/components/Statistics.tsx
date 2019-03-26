22173a18a9281eb45634bb342f25fd5a9eb22944
Harry Green <harry@hgreen.co.uk>
2019-03-26 20:28:53 +1300

    Update Statistics component

--- src/components/Statistics.tsx
+++ src/components/Statistics.tsx
@@ -14,15 +14,31 @@ const Statistics = () => {
           new Set()
         );
         const authorCount = uniqueAuthors.size;
+        const mostRecent = books.reduce((mostRecent, book) => {
+          if (
+            book.dateRead &&
+            new Date(book.dateRead) > new Date(mostRecent.dateRead || 0)
+          ) {
+            mostRecent = book;
+            return mostRecent;
+          }
+          return mostRecent;
+        }, books[0]);
         return (
           <section style={{ padding: "0.75rem 1rem" }}>
             <h1>Statistics</h1>
-            <dl>
-              <dt>Books</dt>
-              <dd>{bookCount}</dd>
-              <dt>Authors</dt>
-              <dd>{authorCount}</dd>
-            </dl>
+            <p>
+              To date, you have read <strong>{bookCount}</strong> books by a
+              total of <strong>{authorCount}</strong> authors.
+            </p>
+            <p>
+              The latest addition to your library was{" "}
+              <strong>{mostRecent.title}</strong> by{" "}
+              <strong>{mostRecent.author}</strong>, which you completed on{" "}
+              <strong>
+                {new Date(mostRecent.dateRead || 0).toLocaleDateString()}
+              </strong>
+            </p>
           </section>
         );
       }}
